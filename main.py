5fe28af5adcdd204d48bd7ce51ccb1c17a1423bb
rohit <rohitgadhwar05@gmail.com>
2020-10-03 11:12:42 +0530

    initial

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ def index():
     return render_template('index.html')
 
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(VideoCamera()),
-                     mimetype='multipart/x-mixed-replace; boundary=frame')
+# def gen(camera):
+#     while True:
+#         frame = camera.get_frame()
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+#
+#
+# @app.route('/video_feed')
+# def video_feed():
+#     return Response(gen(VideoCamera()),
+#                      mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
